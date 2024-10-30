359115816e8282883083ab82cf6c3734fc74bdb8
astha-gh <asthadevadiga7@gmail.com>
2024-10-30 08:18:27 +0530

    Method GET

--- Topics/methodget.js
+++ Topics/methodget.js
@@ -3,6 +3,10 @@ const app = express();
 let {people} = require('./data');
 
 
+//Get method => default method used to read data
+app.get('/api/people' , (req , res) => {
+    res.status(200).json({success : true , data : people});
+})
 
 app.listen(3000 , () => {
     console.log("Listening on port 3000");
